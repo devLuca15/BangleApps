e479feee47d41733d96ab5da63139317f95b7fdb
Luca C <95968553+devLuca15@users.noreply.github.com>
2022-06-22 10:59:42 +0200

    Update app.js

--- apps/activity/app.js
+++ apps/activity/app.js
@@ -1,50 +1,47 @@
-NRF.on('connect', function () {
-  console.log("connected!")
+NRF.on("connect", function () {
+  console.log("connected!");
   g.clear();
-g.drawString("Connected!");
-})
+  g.drawString("Connected!");
+});
 
-
-NRF.on('disconnect', function () {
+NRF.on("disconnect", function () {
   g.clear();
-g.drawString("Disconnected :(");
-})
+  g.drawString("Disconnected :(");
+});
 
 // Display Hello World!
 g.clear();
-g.setFont('Vector', 20);
+g.setFont("Vector", 20);
 g.drawString(E.getBattery());
 
 let timer = 0;
 
-setInterval(function() {
-  timer++
+setInterval(function () {
+  timer++;
 }, 1000);
 
 let getTimer = () => {
-  g.drawString(timer)
-  return timer
-}
-
-
+  g.drawString(timer);
+  return timer;
+};
 
 NRF.setServices({
   "f8b23a4d-89ad-4220-8c9f-d81756009f0c": {
     "f8b23a4d-89ad-4220-8c9f-d81756009f0c": {
       notify: true,
-        readable: true,
-        value: [timer]
-    }
-  }
-})
-
- NRF.setServices({
-    // Battery level service
-    0x2A19: {
-      0x2A19: {
-        notify: true,
-        readable: true,
-        value: [E.getBattery()]
-      }
+      readable: true,
+      value: [timer],
     },
- })
+  },
+});
+
+NRF.setServices({
+  // Battery level service
+  "6e400001-b5a3-f393-e0a9-e50e24dcca9e": {
+    "6e400001-b5a3-f393-e0a9-e50e24dcca9e": {
+      notify: true,
+      readable: true,
+      value: [E.getBattery()],
+    },
+  },
+});
