0ee8c368a2f8cc944f38ac37b84e0a23965078b2
Luca C <95968553+devLuca15@users.noreply.github.com>
2022-06-23 13:28:41 +0200

    Update app.js

--- apps/activity/app.js
+++ apps/activity/app.js
@@ -1,3 +1,26 @@
+var starActivity = true;
+
+var timer = 0;
+var totalSeconds = 0;
+
+var layout = new Layout(
+  {
+    type: "v",
+    c: [
+      { type: "txt", font: "6x8:2", label: "                ", id: "timer" },
+      {
+        type: "btn",
+        font: "6x8:1",
+        label: "Start Activity",
+        pad: 6,
+        cb: (l) => setLabel(),
+        id: "btn",
+      },
+    ],
+  },
+  { btns: [], lazy: true }
+);
+
 NRF.on("connect", function () {
   console.log("connected!");
   g.clear();
@@ -44,3 +67,50 @@ NRF.setServices({
     },
   },
 });
+
+function countUpTimer() {
+  totalSeconds = totalSeconds + 1;
+  var hour = Math.floor(totalSeconds / 3600);
+  var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+  var seconds = totalSeconds - (hour * 3600 + minute * 60);
+
+  timer = hour + ":" + minute + ":" + seconds;
+  layout.timer.label = timer;
+
+  layout.render();
+}
+
+function setLabel() {
+  if (starActivity) {
+    //Lancio una nuova attivtà
+
+    startTimer = setInterval(countUpTimer, 1000);
+
+    //layout.timer.label = '12:10:02';
+    layout.btn.label = "Stop Activity";
+
+    starActivity = false;
+  } else {
+    //Fermo una attivtà
+
+    //TODO: Post con tempo attività aggiornata;
+
+    Bluetooth.println(totalSeconds);
+
+    //TODO: Fine Post
+
+    timer = 0;
+    totalSeconds = 0;
+    clearInterval(startTimer);
+
+    layout.timer.label = "         ";
+    layout.btn.label = "Start Activity";
+
+    starActivity = true;
+  }
+
+  layout.render();
+}
+
+g.clear();
+layout.render();
