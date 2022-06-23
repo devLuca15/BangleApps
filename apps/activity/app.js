b31756e1f414617682db154567943df8a5ffb9b6
Luca C <95968553+devLuca15@users.noreply.github.com>
2022-06-23 13:39:34 +0200

    Update app.js

--- apps/activity/app.js
+++ apps/activity/app.js
@@ -21,53 +21,6 @@ var layout = new Layout(
   { btns: [], lazy: true }
 );
 
-NRF.on("connect", function () {
-  console.log("connected!");
-  g.clear();
-  g.drawString("Connected!");
-});
-
-NRF.on("disconnect", function () {
-  g.clear();
-  g.drawString("Disconnected :(");
-});
-
-// Display Hello World!
-g.clear();
-g.setFont("Vector", 20);
-g.drawString(E.getBattery());
-
-let timer = 0;
-
-setInterval(function () {
-  timer++;
-}, 1000);
-
-let getTimer = () => {
-  g.drawString(timer);
-  return timer;
-};
-
-NRF.setServices({
-  "f8b23a4d-89ad-4220-8c9f-d81756009f0c": {
-    "f8b23a4d-89ad-4220-8c9f-d81756009f0c": {
-      notify: true,
-      readable: true,
-      value: [timer],
-    },
-  },
-});
-
-NRF.setServices({
-  0x2a19: {
-    0x2a19: {
-      notify: true,
-      readable: true,
-      value: [E.getBattery()],
-    },
-  },
-});
-
 function countUpTimer() {
   totalSeconds = totalSeconds + 1;
   var hour = Math.floor(totalSeconds / 3600);
@@ -114,3 +67,50 @@ function setLabel() {
 
 g.clear();
 layout.render();
+
+NRF.on("connect", function () {
+  console.log("connected!");
+  g.clear();
+  g.drawString("Connected!");
+});
+
+NRF.on("disconnect", function () {
+  g.clear();
+  g.drawString("Disconnected :(");
+});
+
+// Display Hello World!
+g.clear();
+g.setFont("Vector", 20);
+g.drawString(E.getBattery());
+
+let timerConnection = 0;
+
+setInterval(function () {
+  timerConnection++;
+}, 1000);
+
+let getTimer = () => {
+  g.drawString(timerConnection);
+  return timerConnection;
+};
+
+NRF.setServices({
+  "f8b23a4d-89ad-4220-8c9f-d81756009f0c": {
+    "f8b23a4d-89ad-4220-8c9f-d81756009f0c": {
+      notify: true,
+      readable: true,
+      value: [timerConnection],
+    },
+  },
+});
+
+NRF.setServices({
+  0x2a19: {
+    0x2a19: {
+      notify: true,
+      readable: true,
+      value: [E.getBattery()],
+    },
+  },
+});
